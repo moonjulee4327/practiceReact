43e97cfb3635f1359f6710208ca6bb820a20ef0d
moonju lee <mjlee4327@gmail.com>
2025-03-07 00:15:36 +0900

    github page setting

--- react-for-beginner/src/Coin.js
+++ react-for-beginner/src/Coin.js
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 function Coin() {
     const [loading, setLoading] = useState(true);
--- react-for-beginner/src/index.js
+++ react-for-beginner/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import Coin from './Coin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
